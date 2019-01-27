45080f5c8875d36d8951bd60356ec89301dc8215
Tobias Thaden <tt@convoyinteractive.com>
2019-01-27 11:14:19 +0100

    docs: initial

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  base: '/griddlecss',
+  title: 'GriddleCSS',
+  themeConfig: {
+       sidebar: 'auto',
+       nav: [
+            { text: 'GitHub', link: 'https://github.com/tobiasthaden/griddlecss' },
+        ]
+    }
+}
